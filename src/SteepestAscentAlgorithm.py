110bba0b9c23cbddf482c373165f55bdce2df82d
arvi <arvianoyuono@gmail.com>
2024-11-02 15:57:00 +0700

    feat(SteepestAscentAlgorithm): adds a feature to support sideways move and fix some error regarding duration and variables naming

--- src/SteepestAscentAlgorithm.py
+++ src/SteepestAscentAlgorithm.py
@@ -7,30 +7,35 @@ from datetime import datetime
 
 class SteepestAscentAlgorithm(BaseAlgorithm):
     @staticmethod
-    def solve(intial_state: State) -> BaseResult:
+    def solve(initial_state: State, sideways_move: bool = False) -> BaseResult:
         """
         This function take initial_state as the input
-        and output the best state obtained using
+        and output the BaseResult object obtained using
         steepest ascent algorithm
         """
         start_time = datetime.now()
+        
         result = BaseResult()
-        if current.value == 0:
-            duration = start_time - datetime.now()
+
+        if initial_state.value == 0:
+            duration = datetime.now() - start_time
             result.add_state(initial_state)
             result.duration(duration)
             return result
         else:
             current_state = initial_state
-
+        
         while True:
-            neighboor = util.getBestSuccessor(intial_state)
+            neighboor = Utility.getBestSuccessor(current_state)
             
-            if neighboor.value <= current.value:
-                duration = start_time - datetime.now()
-                result.add_state(current_state)
+            if not sideways_move and neighboor.value <= current_state.value:
+                duration = datetime.now() - start_time
                 result.duration(duration)
                 return result
-            
+            if sideways_move and neighboor.value < current_state.value:
+                duration = datetime.now() - start_time
+                result.duration(duration)
+                return result
+
             result.add_state(current_state)
-            current = neighboor
+            current_state = neighboor
