1beb7aa82c89c3460e9ec7712a5b09ad24cc2637
arvi <arvianoyuono@gmail.com>
2024-11-02 16:35:17 +0700

    feat: add random restart

---
+++ src/RandomRestartAlgorithm.py
@@ -0,0 +1,12 @@
+from SteepestAscentAlgorithm import SteepestAscentAlgorithm as steep
+from State import State
+from BaseResult import BaseResult
+
+class RandomRestart(steep):
+    def solve(initial_state: State) -> BaseResult:        
+        """
+        This function take initial_state as the input
+        and output the BaseResult object obtained using
+        random restart algorithm
+        """
+        return super().solve(initial_state, random_restart = True)        
