636f15840d3fe9549b1dbdec5983d3e7a53a591d
arvi <arvianoyuono@gmail.com>
2024-11-02 16:34:51 +0700

    feat(SteepestAscentAlgorithm): add random restart implementation

--- src/SteepestAscentAlgorithm.py
+++ src/SteepestAscentAlgorithm.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class SteepestAscentAlgorithm(BaseAlgorithm):
     @staticmethod
-    def solve(initial_state: State, sideways_move: bool = False) -> BaseResult:
+    def solve(initial_state: State, sideways_move: bool = False, random_restart: bool = False) -> BaseResult:
         """
         This function take initial_state as the input
         and output the BaseResult object obtained using
@@ -27,15 +27,26 @@ class SteepestAscentAlgorithm(BaseAlgorithm):
         
         while True:
             neighboor = Utility.getBestSuccessor(current_state)
-            
+
+            #Steepest Ascent
             if not sideways_move and neighboor.value <= current_state.value:
                 duration = datetime.now() - start_time
                 result.duration(duration)
                 return result
+            # Sideways Move
             if sideways_move and neighboor.value < current_state.value:
                 duration = datetime.now() - start_time
                 result.duration(duration)
                 return result
+            #Random restart
+            if random_restart and neighboor.value < current_state.value:
+                neighboor = Utility.getRandomSuccessor(current_state)
 
             result.add_state(current_state)
             current_state = neighboor
+            
+            #Global maximum
+            if current_state.value == 0:
+                duration = datetime.now() - start_time
+                result.duration(duration)
+                return result
