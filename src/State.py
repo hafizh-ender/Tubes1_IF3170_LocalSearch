fcb118ca49dc16472375ce86bd846da7284aef35
Hafizh Renanto Akhmad <hafizhrenanto88@gmail.com>
2024-11-03 16:55:50 +0700

    perf(State): optimize `calculateObjectiveValue` function
    optimize `calculateObjectiveFunction` with the help of ChatGPT and add static typing

--- src/State.py
+++ src/State.py
@@ -3,95 +3,86 @@ import copy
 
 from Cube import Cube
 
-class State():
-    def __init__(self, cube):
+class State:
+    def __init__(self, cube: Cube):
         self._cube = cube
         self._value = self.calculateObjectiveValue()
 
-    # Getter and Setter for cube
+    def __lt__(self, other):
+        return self._value < other._value
+
+    def __le__(self, other):
+        return self._value <= other._value
+
+    def __eq__(self, other):
+        return self._value == other._value
+
+    def __ne__(self, other):
+        return self._value != other._value
+
+    def __gt__(self, other):
+        return self._value > other._value
+
+    def __ge__(self, other):
+        return self._value >= other._value
+
     @property
     def cube(self):
         return self._cube
 
     @cube.setter
-    def cube(self, new_cube):
-        if isinstance(new_cube, Cube):
-            self._cube = new_cube
-            self._value = self.calculateObjectiveValue()  # Recalculate value when cube is updated
-        else:
-            raise ValueError("cube must be an instance of the Cube class")
-
-    # Getter for value (no setter as it is calculated)
+    def cube(self, new_cube: Cube):
+        self._cube = new_cube
+        self._value = self.calculateObjectiveValue()  # Recalculate value when cube is updated
+        
     @property
     def value(self):
         return self._value
 
     # Calculate objective value
-    def calculateObjectiveValue(self):        
+    def calculateObjectiveValue(self) -> int:        
+        dim = self._cube.dim
+        magic_number = self._cube.magic_number
+
+        # Convert values to a numpy array for efficient slicing and summation
+        values = np.array(self._cube.values)
+
+        # Initialize conflict count
         conflicting = 0
-        
-        # Check rows
-        for i in range(self._cube.dim):
-            for k in range(self._cube.dim):
-                sum_baris = sum(self._cube.values[i][j][k] for j in range(self._cube.dim))
-                if sum_baris != self._cube.magic_number:
-                    conflicting += 1
-        
-        # Check columns
-        for j in range(self._cube.dim):
-            for k in range(self._cube.dim):
-                sum_kolom = sum(self._cube.values[i][j][k] for i in range(self._cube.dim))
-                if sum_kolom != self._cube.magic_number:
-                    conflicting += 1
-        
-        # Check pillars
-        for i in range(self._cube.dim):
-            for j in range(self._cube.dim):
-                sum_tiang = sum(self._cube.values[i][j][k] for k in range(self._cube.dim))
-                if sum_tiang != self._cube.magic_number:
-                    conflicting += 1    
-                    
-        # Check space diagonals
-        lower_corners = [[1,1,1],[self._cube.dim,1,1],[self._cube.dim,self._cube.dim,1],[1,self._cube.dim,1]]
-        for lower_corner in lower_corners:
-            sum_diag_ruang = 0
-            i_range = range(0, self._cube.dim) if lower_corner[0] == 1 else range(self._cube.dim-1, -1, -1)
-            j_range = range(0, self._cube.dim) if lower_corner[1] == 1 else range(self._cube.dim-1, -1, -1)
-            k_range = range(0, self._cube.dim) if lower_corner[2] == 1 else range(self._cube.dim-1, -1, -1)
-            
-            for i, j, k in zip(i_range, j_range, k_range):
-                sum_diag_ruang += self._cube.values[i][j][k]
-            
-            if sum_diag_ruang != self._cube.magic_number:
-                conflicting += 1 
-        
-        # Check plane diagonals
-        lower_corners = [[1,1],[1,self._cube.dim]]
-        for i in range(self._cube.dim):
-            for lower_corner in lower_corners:
-                sum_diag_bidang = sum(self._cube.values[i][j][k] for j, k in zip(
-                    range(0, self._cube.dim) if lower_corner[0] == 1 else range(self._cube.dim-1, -1, -1),
-                    range(0, self._cube.dim) if lower_corner[1] == 1 else range(self._cube.dim-1, -1, -1)
-                ))
-                if sum_diag_bidang != self._cube.magic_number:
-                    conflicting += 1 
-                                
-        for j in range(self._cube.dim):
-            for lower_corner in lower_corners:
-                sum_diag_bidang = sum(self._cube.values[i][j][k] for i, k in zip(
-                    range(0, self._cube.dim) if lower_corner[0] == 1 else range(self._cube.dim-1, -1, -1),
-                    range(0, self._cube.dim) if lower_corner[1] == 1 else range(self._cube.dim-1, -1, -1)
-                ))
-                if sum_diag_bidang != self._cube.magic_number:
-                    conflicting += 1 
-                    
-        for k in range(self._cube.dim):
-            for lower_corner in lower_corners:
-                sum_diag_bidang = sum(self._cube.values[i][j][k] for j, i in zip(
-                    range(0, self._cube.dim) if lower_corner[0] == 1 else range(self._cube.dim-1, -1, -1),
-                    range(0, self._cube.dim) if lower_corner[1] == 1 else range(self._cube.dim-1, -1, -1)
-                ))
-                if sum_diag_bidang != self._cube.magic_number:
-                    conflicting += 1 
-            
+
+        # Check row sums (across the x-axis), column sums (y-axis), and pillar sums (z-axis)
+        row_sums = values.sum(axis=2)
+        col_sums = values.sum(axis=0)
+        pillar_sums = values.sum(axis=1)
+
+        # Count conflicts for rows, columns, and pillars
+        conflicting += np.sum(row_sums != magic_number)
+        conflicting += np.sum(col_sums != magic_number)
+        conflicting += np.sum(pillar_sums != magic_number)
+
+        # Space diagonals
+        if np.trace(values, axis1=0, axis2=1).sum() != magic_number:
+            conflicting += 1
+        if np.trace(values[::-1], axis1=0, axis2=1).sum() != magic_number:
+            conflicting += 1
+        if np.trace(values[:, ::-1, :]).sum() != magic_number:
+            conflicting += 1
+        if np.trace(values[:, :, ::-1]).sum() != magic_number:
+            conflicting += 1
+
+        # Plane diagonals in xy, yz, and xz planes (forward and reverse)
+        for i in range(dim):
+            if values[i].diagonal().sum() != magic_number:  # xy-plane
+                conflicting += 1
+            if values[:, i, :].diagonal().sum() != magic_number:  # yz-plane
+                conflicting += 1
+            if values[:, :, i].diagonal().sum() != magic_number:  # xz-plane
+                conflicting += 1
+            if values[i, :, ::-1].diagonal().sum() != magic_number:  # xy-plane, reverse
+                conflicting += 1
+            if values[:, i, ::-1].diagonal().sum() != magic_number:  # yz-plane, reverse
+                conflicting += 1
+            if values[::-1, :, i].diagonal().sum() != magic_number:  # xz-plane, reverse
+                conflicting += 1
+
         return -conflicting
