d4b5bf35dba876887976a460b2833baa4af04c6b
Hafizh Renanto Akhmad <hafizhrenanto88@gmail.com>
2024-11-03 13:47:49 +0700

    fix(SimulatedAnnealingResult): add local optima 'stuck' frequency

--- src/SimulatedAnnealingResult.py
+++ src/SimulatedAnnealingResult.py
@@ -7,11 +7,15 @@ class SimulatedAnnealingResult(BaseResult):
         self._deltaE_history = []
         self._temperature_history = []
         self._exponent_history = []
+        self._iteration_stuck_local_optima = 0
         
     def add_exponent(self, deltaE, temperature):
         self._deltaE_history.append(deltaE)
         self._temperature_history.append(temperature)
         self._exponent_history.append(min(np.exp(deltaE/temperature),1))
+        
+        if deltaE < 0:
+            self._iteration_stuck_local_optima += 1
 
     @property
     def deltaE_history(self):
@@ -24,4 +28,9 @@ class SimulatedAnnealingResult(BaseResult):
     @property
     def exponent_history(self):
         return self._exponent_history
+    
+    @property
+    def iteration_stuck_local_optima(self):
+        return self._iteration_stuck_local_optima
+    
     
